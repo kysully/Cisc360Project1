8fead0e1b0e726dfbb88a89dead52e89bf683e3f
kmsully <kmsully@udel>
2014-09-20 22:10:08 -0400

    Added class fields

--- WindowsFormsApplication2/GeminiCore/Memory.cs
+++ WindowsFormsApplication2/GeminiCore/Memory.cs
@@ -11,5 +11,13 @@ namespace GeminiCore
 {
     public class Memory
     {
+        List<short> Instructions;
+        int[] Memory;
+
+        public Memory()
+        {
+            Instructions = new List<short>(10); // Default instruction size is 10
+            Memory = new int[256]; // Default memory size is 256
+        }
     }
 }
